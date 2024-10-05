d28bf4bdb4dd36475f7764b1de483abef225eb18
Karimi-F <fridah37karimi@gmail.com>
2024-10-05 16:57:44 +0300

    Completes the events listening lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('Hee Hee That tickles!');
+}
+
+input.addEventListener('click', clickAlert);
 }
